d9a9bd580dcd8c06f840f807ea717725d99cebba
leeweisberger <lee@airplane.dev>
2022-02-16 10:48:32 -0500

    changed the task

--- testtest.js
+++ testtest.js
@@ -7,7 +7,7 @@ export default async function(params) {
   // You can return data to show outputs to users.
   // Outputs documentation: https://docs.airplane.dev/tasks/outputs
   return [
-    {element: 'hydrogen', weight: 1.008},
+    {element: 'hydrogen!!', weight: 1.008},
     {element: 'helium', weight: 4.0026},
   ];
 }
